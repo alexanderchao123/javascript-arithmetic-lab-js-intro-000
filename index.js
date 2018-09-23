045aa45277885bf795fdc5e8e6e67fa196b5f0ff
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-23 02:40:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,5 @@ function inc(num) { return num += 1 }
 function dec(num) { return num -= 1 }
 
 function makeInt(num) { return parseInt(num, 10) }
+
+function preserveDecimal(num) { return parseFloat(num)}
